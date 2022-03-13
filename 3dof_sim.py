8589d57f45519057dec0d57a7e8663cc761322b9
pistonsoup <62525293+pistonsoup@users.noreply.github.com>
2022-03-12 21:56:15 -0800

    Update 3dof_sim.py

--- 3dof_sim.py
+++ 3dof_sim.py
@@ -1,3 +1,6 @@
+# Change Log
+#Added pid loop for altitude needs tuning and variable gains
+
 from scipy import interpolate
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,15 +9,16 @@ import matplotlib.pyplot as plt
 
 def stateDerivative(state_vector,input_vector,g=9.8055):
     [x,y,x_dot,y_dot,theta,theta_dot] = state_vector
-    [F,M] = input_vector # force in the body and moment about cg
+    [F,M] = input_vector #Force in the body and moment about cg
     theta_ddot = M / 0.00939 #M/I
     x_ddot = F*np.sin(theta)/0.82 #Acceleration = Force/Mass
-    y_ddot = F*np.cos(theta)/0.82 -g
+    y_ddot = F*np.cos(theta)/0.82-g
     return np.array([x_dot,y_dot,x_ddot,y_ddot,theta_dot,theta_ddot])
 
 def thrust(pwm):
-    max_motor_thrust = 470*2 #grams
-    return max_motor_thrust/2000*(pwm-1000)
+    pwm_percent = (pwm-1000)/1000
+    max_motor_thrust = (470*2)/1000 #kilo/grams
+    return max_motor_thrust*pwm_percent*kg2n
 
 # 4th Order Runge Kutta Calculation
 def RK4(x,u,dt):
@@ -22,41 +26,88 @@ def RK4(x,u,dt):
     # Returns: x[k+1]
     # Calculate slope estimates
     K1 = stateDerivative(x, u)
-    # K2 = stateDerivative(x + K1 * dt / 2, u)
-    # K3 = stateDerivative(x + K2 * dt / 2, u)
-    # K4 = stateDerivative(x + K3 * dt, u)
+    K2 = stateDerivative(x + K1 * dt / 2, u)
+    K3 = stateDerivative(x + K2 * dt / 2, u)
+    K4 = stateDerivative(x + K3 * dt, u)
     # Calculate x[k+1] estimate using combination of slope estimates
-    x_next = x + (K1*dt)#1/6 * (K1+ 2*K2 + 2*K3 + K4) * dt
-    return x_next
+    x_next = x + (1/6 * (K1+ 2*K2 + 2*K3 + K4) * dt)
+    return np.array(x_next)
+
+
+
 
 #######
 # Magic Numbers
 in2m = 0.0254
-# Vehicle Properties
+kg2n= 9.8055
 
+# Vehicle Properties
 arm_length = 5 *in2m
 dt=0.002
+control_dt = 1/250
 sim_t = np.arange(0,100,dt)
-
-pwm_input =1010.431383
+pwm_input = 1000
 
 #Inital State
 state_itt = np.zeros((len(sim_t),6))
 state = [0,0,0,0,0,0]
 
+#Control
+kp_alt = 0.2
+ki_alt = 0.001
+kd_alt = 0
+alt_i = 0
+prev_i = 0
+prev_alt_error =0
+alt_output = 1000
+control_timer =0
+#Data Log
+thrust_log = np.zeros(len(sim_t))
+alt_output_log = []
+control_log_time = []
 for i in range (0,len(sim_t)):
     #do a bunch of random voodo
     #get the hoodo
     #sim results
-    m1_force = thrust(pwm_input)
-    m2_force = thrust(pwm_input)
+
+    if i-control_timer >= control_dt:
+        #alltitude pid
+        alt_error = 10-state[1]
+        if alt_error > 10:
+            kp_alt = 3
+        alt_p = kp_alt*alt_error
+        alt_i = prev_i + ki_alt*alt_error*dt
+        prev_i = alt_i
+        alt_d = kd_alt*(prev_alt_error - alt_error)/dt
+        prev_alt_error = alt_d
+        alt_output = alt_p+alt_i+alt_d
+        alt_output_log.append(alt_output)
+        if alt_output > 2000:
+            alt_output = 2000
+        elif alt_output < 1000:
+            alt_output = 1000
+        control_log_time.append(i)
+        control_timer = i
+
+
+
+    m1_force = thrust(alt_output)
+    m2_force = thrust(alt_output)
     M = m1_force*arm_length - m2_force*arm_length
 
     if i>0:
         state = state_itt[i-1][:]
-    state_itt[i][:] = np.array(RK4(np.array(state),np.array([m1_force+m2_force,M]),dt))
+
+    state_itt[i][:] = RK4(np.array(state),np.array([m1_force+m2_force,M]),dt)
 
 
+
+
+plt.plot(control_log_time, alt_output_log, 'r--', label="pid_output")
+plt.legend()
+plt.xlabel('Time(s)')
+plt.ylabel('Pid Output')
+plt.show()
 
 plt.plot(sim_t, state_itt[:,0], 'r--', label="x_pos")
 plt.plot(sim_t, state_itt[:,1], 'k--', label="y_pos")
