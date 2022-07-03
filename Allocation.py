d67cdde74271b29fc15f13e353f315109179b35f
pistonsoup <62525293+pistonsoup@users.noreply.github.com>
2022-07-03 15:19:19 -0700

    Revert "Add files via upload"
    This reverts commit 8fb566268bfafbd2db08ead18b0083fb0d30cf29.

--- Allocation.py
+++
@@ -1,6 +0,0 @@
-# file for allocation
-
-def allocator(pid_output):
-    #ALLOCATE!
-
-    return None
--- Navigation.py
+++
@@ -1,8 +0,0 @@
-# File for navigation
-import strapdown
-
-
-def navigation(C_B2I,gyro,accelerometer,dt):
-
-    Cdot_B2I = strapdown.rates2dcm(C_B2I,gyro)
-    C_B2I = C_B2I + C_B2I*dt
